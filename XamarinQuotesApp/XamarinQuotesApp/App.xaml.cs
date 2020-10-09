70c225f95b2480d456679771f4209b6d3715e0d5
Zuhair Haroon Khan <zk222ac@gmail.com>
2020-10-10 01:42:48 +0200

    Add project files.

---
+++ XamarinQuotesApp/XamarinQuotesApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinQuotesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new QuotePage();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamarinQuotesApp/XamarinQuotesApp/QuotePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinQuotesApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotePage : ContentPage
+    {
+        private int index = 0;
+        private string[] quotes = new string[] 
+        {
+            "Life is like riding a bicycle. To keep your balance , you must keep moving",
+            "You can,t blame gravity for falling in love" ,
+            "Look deep into nature, and then you will understand everything better"
+        };
+        public QuotePage()
+        {
+            InitializeComponent();
+            currentQuote.Text = quotes[index];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            index++;
+            if(index >= quotes.Length)
+            {
+                index = 0;
+            }
+            currentQuote.Text = quotes[index];
+        }
+    }
+}
